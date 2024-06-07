d8c111e18d8ee6a746dfe94d6e194e05f255b18c
Filip Troníček <filip@gitpod.io>
2024-06-07 15:06:38 +0200

    Revert "Disable signature verification for now"
    This reverts commit da546409a49eafc1a30a663c0443f44b9e87d799.

--- src/vs/platform/extensionManagement/node/extensionDownloader.ts
+++ src/vs/platform/extensionManagement/node/extensionDownloader.ts
@@ -115,8 +115,7 @@ export class ExtensionsDownloader extends Disposable {
 		}
 
 		const value = this.configurationService.getValue('extensions.verifySignature');
-		// for now we disable signature verification until we properly test it with the Open VSX deployment
-		return false ?? (isBoolean(value) ? value : true);
+		return isBoolean(value) ? value : true;
 	}
 
 	private async downloadVSIX(extension: IGalleryExtension, operation: InstallOperation): Promise<URI> {
